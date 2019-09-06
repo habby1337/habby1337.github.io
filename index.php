555aa135a307cfa2cf846c00e7e21c84a5b541be
Federico Tensi <federicotensi@gmail.com>
2019-09-06 23:50:46 +0200

    5

---
+++ index.php
@@ -0,0 +1,395 @@
+<!DOCTYPE html>
+<html lang="it">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <title>Home - Federico Tensi</title>
+    <meta name="description" content="Web and Desktop Developer">
+    <link rel="shortcut icon" type="image/png" href="img/favicon.ico" />
+    <meta name="keywords" content="HTML,CSS,XML,JavaScript,Developer,C#,Linux,Programmer">
+    <meta name="author" content="Federico Tensi">
+    <!-- Bootstrap core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom fonts for this template -->
+    <link href="https://fonts.googleapis.com/css?family=Saira+Extra+Condensed:500,700" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Muli:400,400i,800,800i" rel="stylesheet">
+    <!-- Quote font -->
+    <link href="https://fonts.googleapis.com/css?family=Dancing+Script&display=swap" rel="stylesheet">
+
+    <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet">
+
+    <!-- Custom styles -->
+    <link href="css/resume.min.css" rel="stylesheet">
+
+    <!-- Custom Loader -->
+    <link href="css/loader.css" rel="stylesheet">
+
+    <!-- Custom footer -->
+    <link href="css/footer.css" rel="stylesheet">
+
+</head>
+
+<body id="page-top" onload="setTimeFun()">
+
+    <div id="donut" class="align-items-center"></div>
+
+    <div style="display:none;" id="page" class="animate-bottom">
+
+        <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="sideNav">
+            <a class="navbar-brand js-scroll-trigger" href="#page-top">
+                <span class="d-block d-lg-none">Federico Tensi</span>
+                <span class="d-none d-lg-block">
+        <img class="img-fluid img-profile rounded-square mx-auto mb-2" src="img/profile.jpg" alt="me" title="Me">
+      </span>
+            </a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link js-scroll-trigger" href="#about">About</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link js-scroll-trigger" href="#sitebuilder">Shop</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link js-scroll-trigger" href="#experience">Esperienza</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link js-scroll-trigger" href="#education">Istruzione</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link js-scroll-trigger" href="#skills">Abilità</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link js-scroll-trigger" href="#interests">Interessi</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link js-scroll-trigger" href="#awards">Obiettivi</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+
+        <div class="container-fluid p-0">
+
+            <section class="resume-section p-3 p-lg-5 d-flex align-items-center" id="about">
+                <div class="w-100">
+                    <h1 class="mb-0">Federico
+          <span class="text-primary">Tensi</span>
+        </h1>
+                    <div class="subheading mb-5">Rome, Italy ·
+                        <a href="https://t.me/habby1337">Telegram</a> ·
+                        <a href="mailto:federicotensi@gmail.com">federicotensi@gmail.com</a>
+                    </div>
+                    <p class="lead mb-5">Ho esperienza con diversi frameworks e alcuni linguaggi desktop, anche una buona leadership e sono in grado di lavorare in team.</p>
+                    <div class="social-icons">
+                        <a href="https://www.linkedin.com/in/federico-tensi-4ba5b4170/">
+                            <i class="fab fa-linkedin-in"></i>
+                        </a>
+                        <a href="https://github.com/habby1337">
+                            <i class="fab fa-github"></i>
+                        </a>
+                        <a href="https://t.me/habby1337">
+                            <i class="fab fa-telegram"></i>
+                        </a>
+                        <a href="https://www.instagram.com/fede.tensi/">
+                            <i class="fab fa-instagram"></i>
+                        </a>
+                    </div>
+                </div>
+            </section>
+
+            <hr class="m-0">
+
+            <section class="resume-section p-3 p-lg-5 d-flex align-items-center" id="sitebuilder">
+                <div class="w-100">
+                    <h2 class="mb-5">Necessiti un sito web?</h2>
+                    <hr class="divider light my-4">
+                    <div class="subheading mb-3">Sono anni che sviluppo siti, se hai bisogno di una mano posso aiutarti :)</div>
+                    <button type="button" class="btn btn-outline-fede" onclick="location.href='https://habby1337.github.io/shop';">Ho bisogno di un sito web!</button>
+            </section>
+
+            <hr class="m-0">
+
+            <section class="resume-section p-3 p-lg-5 d-flex justify-content-center" id="experience">
+                <div class="w-100">
+                    <h2 class="mb-5">Esperienza</h2>
+
+                    <!-- Adrenaline gaming -->
+                    <div class="resume-item d-flex flex-column flex-md-row justify-content-between mb-5">
+                        <div class="resume-content">
+                            <h3 class="mb-0">Technician</h3>
+                            <div class="subheading mb-3">Adrenaline Gaming</div>
+                            <p>The Adrenaline Multigaming Clan is a place for gamers from around the world. fondata nel 2017</p>
+                        </div>
+                        <div class="resume-date text-md-right">
+                            <span class="text-primary">Luglio 2019 - Oggi</span>
+                        </div>
+                    </div>
+
+                    <!-- Redignus RE -->
+                    <div class="resume-item d-flex flex-column flex-md-row justify-content-between mb-5">
+                        <div class="resume-content">
+                            <h3 class="mb-0">Senior Web Developer</h3>
+                            <div class="subheading mb-3">Redignus RE</div>
+                            <p>REDignus è un registro elettronico basato su Axios che implementa notevoli migliorie.</p>
+                        </div>
+                        <div class="resume-date text-md-right">
+                            <span class="text-primary">Settembre 2018 - Oggi</span>
+                        </div>
+                    </div>
+
+                    <!-- Sanik Multigaming -->
+                    <div class="resume-item d-flex flex-column flex-md-row justify-content-between mb-5">
+                        <div class="resume-content">
+                            <h3 class="mb-0">Senior Web Developer</h3>
+                            <div class="subheading mb-3">Sanik Multigaming</div>
+                            <p>Una community dove tutti i player online potevano giocare insieme, fare tornei o semplicemente divertirsi fra loro.</p>
+                        </div>
+                        <div class="resume-date text-md-right">
+                            <span class="text-primary">Agosto 2016 - Luglio 2018</span>
+                        </div>
+                    </div>
+
+                </div>
+
+            </section>
+
+            <hr class="m-0">
+
+            <section class="resume-section p-3 p-lg-5 d-flex align-items-center" id="education">
+                <div class="w-100">
+                    <h2 class="mb-5">Istruzione</h2>
+
+                    <div class="resume-item d-flex flex-column flex-md-row justify-content-between mb-5">
+                        <div class="resume-content">
+                            <h3 class="mb-0">IIS Federico Caffè</h3>
+                            <div class="subheading mb-3">Informatica e Telecomunicazioni</div>
+                            <div>Sistemi e Reti, Telecomunicazioni, Progettazione di sistemi, Programmazione.</div>
+                            <!-- <p>GPA: 3.23</p> -->
+                        </div>
+                        <div class="resume-date text-md-right">
+                            <span class="text-primary">2015 - Oggi</span>
+                        </div>
+                    </div>
+
+                </div>
+            </section>
+
+            <hr class="m-0">
+
+            <section class="resume-section p-3 p-lg-5 d-flex align-items-center" id="skills">
+                <div class="w-100">
+                    <h2 class="mb-5">Abilità</h2>
+
+                    <div class="subheading mb-3">Linguaggi di Programmazione &amp; Strumenti</div>
+                    <ul class="list-inline dev-icons">
+                        <li class="list-inline-item">
+                            <i class="fab fa-html5"></i>
+                        </li>
+                        <li class="list-inline-item">
+                            <i class="fab fa-css3-alt"></i>
+                        </li>
+                        <li class="list-inline-item">
+                            <i class="fab fa-js-square"></i>
+                        </li>
+                        <li class="list-inline-item">
+                            <i class="fab fa-angular"></i>
+                        </li>
+                        <li class="list-inline-item">
+                            <i class="fab fa-react"></i>
+                        </li>
+                        <li class="list-inline-item">
+                            <i class="fab fa-php"></i>
+                        </li>
+                        <li class="list-inline-item">
+                            <i class="fab fa-sass"></i>
+                        </li>
+                        <li class="list-inline-item">
+                            <i class="fab fa-less"></i>
+                        </li>
+                        <li class="list-inline-item">
+                            <i class="fab fa-wordpress"></i>
+                        </li>
+                        <li class="list-inline-item">
+                            <i class="fab fa-linux"></i>
+                        </li>
+                        <li class="list-inline-item" style="font-size: 0.9em;">
+                            <i class="fab fa-cuttlefish"><i class="fas fa-hashtag"></i></i>
+                        </li>
+                        <li class="list-inline-item">
+                            <i class="fab fa-python"></i>
+                        </li>
+                    </ul>
+
+                    <div class="subheading mb-3">Workflow</div>
+                    <ul class="fa-ul mb-0">
+                        <li>
+                            <i class="fa-li fa fa-check"></i> Mobile-First, Responsive Design</li>
+                        <li>
+                            <i class="fa-li fa fa-check"></i> Cross Browser Testing &amp; Debugging</li>
+                        <li>
+                            <i class="fa-li fa fa-check"></i> Cross Functional Teams</li>
+                        <li>
+                            <i class="fa-li fa fa-check"></i> Agile Development</li>
+                    </ul>
+                </div>
+            </section>
+
+            <hr class="m-0">
+
+            <section class="resume-section p-3 p-lg-5 d-flex align-items-center" id="interests">
+                <div class="w-100">
+                    <h2 class="mb-5">Interessi</h2>
+                    <p>A parte la programmazione web o desktop, Mi piace passare la maggior parte del mio tempo fuori all'aperto in moto. Qualsiasi sia la stagione. Sono un motociclista appassionato.</p>
+                    <p class="mb-0">Quando sono costretto a stare in casa, gioco a qualche titolo con dei miei amici oppure guardo delle serie TV, mi piace anche cucinare, e spendo una gran parte del mio tempo libero a esplorare le ultime novità tecnologiche.</p>
+                </div>
+            </section>
+
+            <hr class="m-0">
+
+            <section class="resume-section p-3 p-lg-5 d-flex align-items-center" id="awards">
+                <div class="w-100">
+                    <h2 class="mb-5">Obiettivi &amp; Certificazioni</h2>
+                    <ul class="fa-ul mb-0">
+                        <li>
+                            <i class="fa-li fa fa-trophy text-warning"></i> Work in Progress</li>
+                        <li>
+                            <!-- <li>
+            <i class="fa-li fa fa-trophy text-warning"></i>
+            Google Analytics Certified Developer</li>
+          <li>
+            <i class="fa-li fa fa-trophy text-warning"></i>
+            Mobile Web Specialist - Google Certification</li>
+          <li>
+            <i class="fa-li fa fa-trophy text-warning"></i>
+            1<sup>st</sup>
+            Place - University of Colorado Boulder - Emerging Tech Competition 2009</li>
+          <li>
+            <i class="fa-li fa fa-trophy text-warning"></i>
+            1<sup>st</sup>
+            Place - University of Colorado Boulder - Adobe Creative Jam 2008 (UI Design Category)</li>
+          <li>
+            <i class="fa-li fa fa-trophy text-warning"></i>
+            2<sup>nd</sup>
+            Place - University of Colorado Boulder - Emerging Tech Competition 2008</li>
+          <li>
+            <i class="fa-li fa fa-trophy text-warning"></i>
+            1<sup>st</sup>
+            Place - James Buchanan High School - Hackathon 2006</li>
+          <li>
+            <i class="fa-li fa fa-trophy text-warning"></i>
+            3<sup>rd</sup>
+            Place - James Buchanan High School - Hackathon 2005</li>
+            -->
+                    </ul>
+                </div>
+            </section>
+
+            </div>
+
+            <!-- Bootstrap core JavaScript -->
+            <script src="vendor/jquery/jquery.min.js"></script>
+            <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+            <!-- Plugin JavaScript -->
+            <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+
+            <!-- Custom scripts for this template -->
+            <script src="js/resume.min.js"></script>
+
+            <!-- Loader -->
+            <script>
+                var timeout;
+                var url = "https://quotes.rest/qod.json?category=inspire";
+
+                function setTimeFun() {
+                    timeout = setTimeout(showPage, 1000);
+
+                    if (window.XMLHttpRequest) { // code for IE7+, Firefox, Chrome, Opera, Safari
+                        xmlhttp = new XMLHttpRequest();
+                    } else { // code for IE6, IE5
+                        xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+                    }
+                    xmlhttp.onreadystatechange = function() {
+                        if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+                            return xmlhttp.responseText;
+                        }
+                    }
+                    xmlhttp.open("GET", url, false);
+                    xmlhttp.send();
+                    alert(xmlhttp.responseText)
+                    var data = JSON.parse(xmlhttp.responseText);
+
+                    alert(data.contents[0].quotes.[0].quote)
+                    alert(data.contents[0].quotes.[0])
+                    alert(data.contents[0])
+
+
+                }
+
+                function showPage() {
+                    document.getElementById("donut").style.display = "none";
+                    document.getElementById("page").style.display = "block";
+
+                }
+            </script>
+
+            <!-- The content of your page would go here. -->
+            <footer class="footer-distributed">
+
+                <div class="footer-right">
+
+                    <a href="https://www.linkedin.com/in/federico-tensi-4ba5b4170/">
+                        <i class="fab fa-linkedin-in"></i>
+                    </a>
+                    <a href="https://github.com/habby1337">
+                        <i class="fab fa-github"></i>
+                    </a>
+                    <a href="https://t.me/habby1337">
+                        <i class="fab fa-telegram"></i>
+                    </a>
+                    <a href="https://www.instagram.com/fede.tensi/">
+                        <i class="fab fa-instagram"></i>
+                    </a>
+
+                </div>
+
+                <p class="quote" id="quoteofday"></p>
+
+                <div class="footer-left">
+
+                    <!-- <p class="footer-links">
+        <a href="#">Home</a>
+        ·
+        <a href="#">Blog</a>
+        ·
+        <a href="#">Pricing</a>
+        ·
+        <a href="#">About</a>
+        ·
+        <a href="#">Faq</a>
+        ·
+        <a href="#">Contact</a>
+      </p> -->
+                    <p class="footer-links">
+                        <p class="mb-0">
+                            Federico Tensi &copy;
+                            <?php echo date("Y"); ?>
+                        </p>
+                    </p>
+                </div>
+
+            </footer>
+
+        </div>
+
+</body>
+
+</html>
